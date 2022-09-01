b481fe38a001ca15ecd88479d0fac8b5e0fede09
Mamatha-Alam <100273197+Mamatha-Alam@users.noreply.github.com>
2022-09-01 16:55:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry OatMeal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Hard Boiled Free range egg')
